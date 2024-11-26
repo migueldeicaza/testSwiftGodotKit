531fe36deeaf508228d47ba4cc91a6d5b4bb0161
Miguel de Icaza <miguel@gnome.org>
2024-11-26 10:36:50 -0500

    Update sample, but this shows the red bug

--- testSwiftGodotKit/ContentView.swift
+++ testSwiftGodotKit/ContentView.swift
@@ -13,11 +13,29 @@ struct ContentView: View {
     @State var app = GodotApp(packFile: "game.pck")
 
     var body: some View {
-        VStack {
-            Text("Game is below:")
-            GodotAppView(app: app)
-                .padding()
+        HStack {
+            VStack {
+                Text("Game is below:")
+                GodotAppView()
+                    .padding()
+            }
+            VStack {
+                Text("An embedded Godot window, with custom code")
+                GodotWindow { (subwindow: Window) -> () in
+                    let ctr = VBoxContainer()
+                    ctr.setAnchorsPreset(Control.LayoutPreset.fullRect)
+                    subwindow.addChild(node: ctr)
+                    
+                    let button1 = Button()
+                    button1.text = "SubWindow 1"
+                    let button2 = Button()
+                    button2.text = "Another Button"
+                    ctr.addChild(node: button1)
+                    ctr.addChild(node: button2)
+                }
+            }
         }
+        .environment(\.godotApp, app)
     }
 }
 
